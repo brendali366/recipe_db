9702f72f824c024564386adb34f016eb4a69e854
Brendali366 <brendali366@gmail.com>
2022-02-24 18:12:35 -0600

    added pg for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :recipes
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'recipes#index'
+  get 'pages/about'
 end
